28b0fe8c694907e0932ea69fc7b1a4ddc48d9b75
BarbaraLima21 <Barbar.200.lima@gmail.com>
2025-03-19 23:32:13 -0300

    Update A03.ex02.c

--- A03.ex02.c
+++ A03.ex02.c
@@ -2,24 +2,46 @@
 
 int main() {
     int a, b, c;
+    int continuar = 1;
 
-    while (1) {
-        printf("Digite três valores inteiros (-1 para sair): ");
+    while (continuar) {
+        printf("Digite o primeiro valor (-1 para sair): ");
         scanf("%d", &a);
-        
+
         if (a == -1) {
-            printf("verificação encerrada\n");
-            
-        }
-
-        scanf("%d %d", &b, &c);
-
-        if ((a * a == b * b + c * c) 
-            (b * b == a * a + c * c) 
-            (c * c == a * a + b * b)) {
-            printf("Os valores formam um triângulo retângulo!\n");
+            continuar = 0;
         } else {
-            printf("Os valores NÃO formam um triângulo retângulo.\n");
+            printf("Digite o segundo valor (-1 para sair): ");
+            scanf("%d", &b);
+
+            printf("Digite o terceiro valor (-1 para sair): ");
+            scanf("%d", &c);
+
+            if (a > 0) {
+             if (b > 0) {
+                if(c > 0) {
+                  if (a * a == b * b + c * c) {
+                      printf("Os valores formam um triângulo retângulo!\n");
+                  } else {
+                      if (b * b == a * a + c * c) {
+                           printf("Os valores formam um triângulo retângulo!\n");
+                      } else {
+                          if(c * c == a * a + b * b){
+                              printf("Os valores formam um triângulo retângulo!\n");
+                          } else{
+                              printf("Os valores NÃO formam um triângulo retângulo.\n");
+                          }
+                      }
+                  }
+                } else {
+                  printf("Os valores digitados não formam um triângulo válido.\n");
+                }
+              } else {
+                printf("Os valores digitados não formam um triângulo válido.\n");
+              }
+            } else {
+              printf("Os valores digitados não formam um triângulo válido.\n");
+            }
         }
     }
     return 0;
